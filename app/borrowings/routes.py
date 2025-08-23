947365bbc4fdc67950c810dc134c128dc7345d6f
Ayesha123413 <ayeshanazar21619@gmail.com>
2025-08-23 22:50:43 +0500

    borrow book

--- app/borrowings/routes.py
+++ app/borrowings/routes.py
@@ -6,3 +6,22 @@ from app.utils.auth_guard import get_current_user, require_role
 
 
 borrow_router = APIRouter()
+
+@borrow_router.post("/",response_model:BorrowBookRequest)
+def borrow_book(payload: BorrowBookRequest,user=Depends(require_role("member"))):
+    book=supabase.table("books").select("*").eq("id",payload.book_id).single().execute
+    if not book.data:
+        raise HTTP_404_NOT_FOUND(status_code:404 , detail="Book not found")
+    if book.data.quantity<=0:
+        raise HTTPException(status_code:400,detail:"Book not available")
+    
+    borrowing_data={
+       " user_id":user.id,
+        "book_id":payload.book_id,
+        "borrow_date":data.today().isofromat()
+        "due_date":payload.due_date.isoformat()
+        "status":"borrowed"
+    }
+    borrowing=supabase.table("borrowing").insert(borrowing_data).execute()
+    supabase.table("books").update({quantity:book.data.quantity-1}).eq("id",payload.book_id).execute()
+    return borrowing.data[0]
